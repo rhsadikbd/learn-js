b8413eb3d3b4f66b9fc1e2fcbd67e5c717f658f2
RH Sadik <sadikcsbd@gmail.com>
2026-02-05 18:11:32 +0600

    updated

--- axios-api-v1/app.js
+++ axios-api-v1/app.js
@@ -1,27 +1,58 @@
+// const fetchData = async (config) => {
+//   const res = await axios(config);
+//   return res.data;
+// };
+// const postsElement = document.querySelector(".posts");
+// const loadAllData = async () => {
+//   const posts = await fetchData("https://jsonplaceholder.typicode.com/posts");
+//   try {
+//     posts.map((post) => {
+//       const postElement = document.createElement("div");
+//       postElement.classList.add("post");
+//       postElement.innerHTML = `
+//       <div class="post">
+//         <h4 class="title">${post.title}</h4>
+//         <p class="desc">
+//             ${post.body}
+//         </p>
+//         </div>
+//       `;
+//       postsElement.appendChild(postElement);
+//     });
+//   } catch (error) {
+//     throw Error("Data not fatched");
+//   }
+// };
+
+// loadAllData();
+
 const fetchData = async (config) => {
-  const res = await axios(config);
-  return res.data;
-};
-const postsElement = document.querySelector(".posts");
-const loadAllData = async () => {
   try {
-    const posts = await fetchData("https://jsonplaceholder.typicode.com/posts");
-    posts.map((post) => {
-      //   const postElement = document.createElement("div");
-      //   postElement.classList.add("post");
-      postsElement.innerHTML = `
-      <div class="post">
-        <h4 class="title">${post.title}</h4>
+    const res = await axios(config);
+    return res.data;
+  } catch (err) {
+    console.log("Error fatching data");
+    return [];
+  }
+};
+
+const postsEl = document.querySelector(".posts");
+
+const loadAllData = async () => {
+  const posts = await fetchData("https://jsonplaceholder.typicode.com/posts");
+
+  posts.forEach((post) => {
+    const postEl = document.createElement("div");
+    postEl.classList.add("post");
+
+    postEl.innerHTML = `
+       <h4 class="title">${post.title}</h4>
         <p class="desc">
             ${post.body}
         </p>
-        </div>
-      `;
-      //   postsElement.appendChild(postElement);
-    });
-  } catch (error) {
-    throw Error("Data not fatched");
-  }
+    `;
+    postsEl.appendChild(postEl);
+  });
 };
 
 loadAllData();
---
+++ vue/todo-app/src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref } from "vue";
+import TaskCom from "./components/TaskCom.vue";
+const tasksArray = ref([
+  {
+    id: 1,
+    title: "Learn Vue JS",
+    completed: true,
+  },
+  {
+    id: 2,
+    title: "Watch netflix",
+    completed: true,
+  },
+  {
+    id: 3,
+    title: "Go shopping",
+    completed: false,
+  },
+  {
+    id: 4,
+    title: "Learn guitar",
+    completed: false,
+  },
+  {
+    id: 5,
+    title: "Send email",
+    completed: false,
+  },
+]);
+</script>
+
+<template>
+  <div id="app">
+    <TaskCom />
+  </div>
+</template>
+
+<style scoped></style>
