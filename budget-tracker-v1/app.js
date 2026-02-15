2e1daa125455865d5a6882fd6aa70f3cba4c49cf
RH Sadik <sadikcsbd@gmail.com>
2026-02-15 15:35:51 +0600

    budget tracker app

--- budget-tracker-v1/app.js
+++ budget-tracker-v1/app.js
@@ -19,10 +19,68 @@ function getExpenses(numberOfExpense) {
 
 function calculateTotalExpenses(expenses) {
   let totalExpenses = 0;
-  for (let i = 0; i <= expenses.length; index++) {
+  for (let index = 0; index < expenses.length; index++) {
     totalExpenses += expenses[index];
   }
   return totalExpenses;
+}
+
+function calculateTax(income, taxrate) {
+  return income * taxrate;
+}
+function calculateNetIncome(income, tax) {
+  return income - tax;
+}
+function calculateBalance(netIncome, totalExpenses) {
+  return netIncome - totalExpenses;
+}
+function calculateSavings(balance, savingsRate) {
+  return balance * savingsRate;
+}
+
+function checkOverSpending(income, totalExpenses) {
+  return totalExpenses > income
+    ? "Warning! you are spending more than your income"
+    : "";
+}
+
+function getFinancialStatus(savings) {
+  let finalStatus = "";
+  if ((savings) => 1000) {
+    finalStatus = "Excellent! you are saving well!";
+  } else if ((savings) => 500) {
+    finalStatus = "Good! you have a decent savings amount.";
+  } else if ((savings) => 100) {
+    finalStatus = "Needs improvement. consider reducing expense";
+  } else {
+    finalStatus = "Critical! your savings are too low!";
+  }
+  return finalStatus;
+}
+
+function displayResult(
+  userName,
+  income,
+  totalExpenses,
+  tax,
+  netIncome,
+  balance,
+  savings,
+  finalStatus,
+) {
+  console.log("----- Budget Tracker App -----");
+  console.log(`User: ${userName}`);
+  console.log(`Total Income: $${income}`);
+  console.log(`Total Expenses: $${totalExpenses}`);
+  console.log(`Tax deducted (10%): $${tax}`);
+  console.log(`Net income after tax: $${netIncome}`);
+  console.log(`Remaining balance: $${balance}`);
+  console.log(`Savings (20% of balance): $${savings}`);
+  console.log(finalStatus);
+  const overSpendingMessage = checkOverSpending(income, totalExpenses);
+  if (overSpendingMessage) {
+    console.log(overSpendingMessage);
+  }
 }
 
 // Main function to the run the budget tracker
@@ -41,33 +99,23 @@ function runBudgettracker() {
   } else {
     const expenses = getExpenses(numberOfExpense);
     const totalExpenses = calculateTotalExpenses(expenses);
-    const tax = income * 0.1;
-    const netIncome = income - tax;
-    const balance = netIncome - totalExpenses;
-    const savings = balance * 0.2;
+    const tax = calculateTax(income, 0.1);
+    const netIncome = calculateNetIncome(income, tax);
+    const balance = calculateBalance(netIncome, totalExpenses);
+    const savings = calculateSavings(balance, 0.2);
 
-    let finalStatus = "";
-    if ((savings) => 1000) {
-      finalStatus = "Excellent! you are saving well!";
-    } else if ((savings) => 500) {
-      finalStatus = "Good! you have a decent savings amount.";
-    } else if ((savings) => 100) {
-      finalStatus = "Needs improvement. consider reducing expense";
-    } else {
-      finalStatus = "Critical! your savings are too low!";
-    }
+    let finalStatus = getFinancialStatus(savings);
 
-    let overSpendingMessage = "";
-    if (totalExpenses > income) {
-      overSpendingMessage = "Warning! you are spending more than your income";
-    }
-
-    console.log("----- Budget Tracker App -----");
-    console.log(`User: ${userName}`);
-    console.log(`Total Income: $${income}`);
-    console.log(`Total Expenses: $${totalExpenses}`);
-    console.log(`Tax deducted (10%): $${tax}`);
-    console.log(`Net income after tax: $${netIncome}`);
+    displayResult(
+      userName,
+      income,
+      totalExpenses,
+      tax,
+      netIncome,
+      balance,
+      savings,
+      finalStatus,
+    );
   }
 }
 
