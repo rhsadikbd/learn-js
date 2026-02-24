88879517580938f9025d200c2f8ae5d85530e78d
RH Sadik <sadikcsbd@gmail.com>
2026-02-24 15:33:46 +0600

    updated

--- budget-tracker-v4/script.js
+++ budget-tracker-v4/script.js
@@ -1,19 +1,24 @@
-const noOfExpenses = document.querySelector("#noOfExpenses").value;
+console.clear();
 const expensesSection = document.querySelector(".expensesSection");
 const startBudgeting = document.querySelector("#start_budgeting");
 const calculateBudget = document.querySelector("#calculateBudget");
-const userNameInput = document.querySelector("#userName");
-const incomeInput = document.querySelector("#totalIncome");
 startBudgeting.addEventListener("click", getExpenses);
-// calculateBudget.addEventListener("click", startCalculateBudget);
+calculateBudget.addEventListener("click", calculateTotalExpenses);
 
 function getExpenses() {
+  const noOfExpenses = document.querySelector("#noOfExpenses").value;
+  if (noOfExpenses <= 0) {
+    alert("Please enter a valid number of expenses.");
+    return;
+  }
+
   for (let i = 1; i <= noOfExpenses; i++) {
     const labelInput = document.createElement("label");
     labelInput.textContent = `Expense ${i}: `;
 
     const inputField = document.createElement("input");
     inputField.type = "number";
+    inputField.id = `expense${i}`;
     inputField.placeholder = "Enter";
     expensesSection.appendChild(labelInput);
     expensesSection.appendChild(inputField);
@@ -21,18 +26,3 @@ function getExpenses() {
   document.querySelector("#calculateBudget").style.display = "block";
   document.querySelector("#clearBudget").style.display = "block";
 }
-
-// function startCalculateBudget() {
-//   let userBudget = {
-//     userName: userNameInput.value,
-//     income: parseInt(incomeInput.value),
-//     expenses: [],
-//     numberOfExpenses: noOfExpenses,
-//     totalExpenses: 0,
-//     tax: 0,
-//     netIncome: 0,
-//     balance: 0,
-//     savings: 0,
-//     finalStatus: 0,
-//   };
-// }
